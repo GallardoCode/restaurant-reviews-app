c6a912b19382d96a2eb0e072ae9f7532004a1027
Ricardo Gallardo <rgallardouk@gmail.com>
2019-06-12 12:06:45 +0100

    Fix: address the linting issues in dbhelper.js

--- app/scripts/dbhelper.js
+++ app/scripts/dbhelper.js
@@ -15,12 +15,13 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) {
         // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
+        // eslint-disable-next-line prefer-destructuring
         const restaurants = json.restaurants;
         callback(null, restaurants);
       } else {
@@ -41,7 +42,7 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        const restaurant = restaurants.find(r => r.id == id);
+        const restaurant = restaurants.find(r => r.id === id);
         if (restaurant) {
           // Got the restaurant
           callback(null, restaurant);
@@ -63,7 +64,7 @@ class DBHelper {
         callback(error, null);
       } else {
         // Filter restaurants to have only given cuisine type
-        const results = restaurants.filter(r => r.cuisine_type == cuisine);
+        const results = restaurants.filter(r => r.cuisine_type === cuisine);
         callback(null, results);
       }
     });
@@ -79,7 +80,9 @@ class DBHelper {
         callback(error, null);
       } else {
         // Filter restaurants to have only given neighborhood
-        const results = restaurants.filter(r => r.neighborhood == neighborhood);
+        const results = restaurants.filter(
+          r => r.neighborhood === neighborhood
+        );
         callback(null, results);
       }
     });
@@ -99,13 +102,13 @@ class DBHelper {
         callback(error, null);
       } else {
         let results = restaurants;
-        if (cuisine != 'all') {
+        if (cuisine !== 'all') {
           // filter by cuisine
-          results = results.filter(r => r.cuisine_type == cuisine);
+          results = results.filter(r => r.cuisine_type === cuisine);
         }
-        if (neighborhood != 'all') {
+        if (neighborhood !== 'all') {
           // filter by neighborhood
-          results = results.filter(r => r.neighborhood == neighborhood);
+          results = results.filter(r => r.neighborhood === neighborhood);
         }
         callback(null, results);
       }
@@ -127,7 +130,7 @@ class DBHelper {
         );
         // Remove duplicates from neighborhoods
         const uniqueNeighborhoods = neighborhoods.filter(
-          (v, i) => neighborhoods.indexOf(v) == i
+          (v, i) => neighborhoods.indexOf(v) === i
         );
         callback(null, uniqueNeighborhoods);
       }
@@ -147,7 +150,7 @@ class DBHelper {
         const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
         const uniqueCuisines = cuisines.filter(
-          (v, i) => cuisines.indexOf(v) == i
+          (v, i) => cuisines.indexOf(v) === i
         );
         callback(null, uniqueCuisines);
       }
@@ -171,9 +174,13 @@ class DBHelper {
   /**
    * Map marker for a restaurant.
    */
+  // eslint-disable-next-line no-unused-vars
   static mapMarkerForRestaurant(restaurant, map) {
     // https://leafletjs.com/reference-1.3.0.html#marker
-    const marker = new L.marker(
+    /* eslint-disable new-cap */
+    // eslint-disable-next-line no-undef
+    const Marker = new L.marker(
+      /* eslint-disable new-cap */
       [restaurant.latlng.lat, restaurant.latlng.lng],
       {
         title: restaurant.name,
@@ -181,8 +188,8 @@ class DBHelper {
         url: DBHelper.urlForRestaurant(restaurant)
       }
     );
-    marker.addTo(newMap);
-    return marker;
+    Marker.addTo(window.newMap);
+    return Marker;
   }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
@@ -195,3 +202,4 @@ class DBHelper {
     return marker;
   } */
 }
+export default DBHelper;
