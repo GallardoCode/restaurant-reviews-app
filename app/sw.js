e410f168dcd131097b18ff40e2625c72db1f7ef0
Ricardo Gallardo <rgallardouk@gmail.com>
2019-06-17 10:08:20 +0100

    Feat: respond fetch requests with matching cache assets

--- app/sw.js
+++ app/sw.js
@@ -28,3 +28,12 @@ self.addEventListener('install', event => {
       })
   );
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
